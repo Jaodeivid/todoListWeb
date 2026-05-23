7680f575d497faa948bbdbdeae036e02dde5f9d1
David Oliver Chambi Villarroel <davidchambivillarroel05@gmail.com>
2026-05-23 17:38:34 -0400

    Agrego de manejo de cache

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,12 +2,14 @@ const express = require('express')
 const router = express.Router()
 const {
   getTasks,
+  getTaskById,
   crearTask,
   actualizarTask,
   eliminarTask
 } = require('../controllers/taskController')
 
 router.get('/', getTasks)
+router.get('/:id', getTaskById)
 router.post('/', crearTask)
 router.put('/:id', actualizarTask)
 router.delete('/:id', eliminarTask)
